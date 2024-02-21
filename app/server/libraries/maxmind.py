c023ab77816ec24acd71aad216c922d1cf2fcc2a
yusepjaelani861 <yusepjaelani861@gmail.com>
2024-02-21 13:25:14 +0700

    fix bug maxmind

--- app/server/libraries/maxmind.py
+++ app/server/libraries/maxmind.py
@@ -10,35 +10,37 @@ class MaxMind:
             city = self.reader_city.city(ip)
             country = self.reader_country.country(ip)
             return {
-                'status': 'success',
-                'ip': ip,
-                'country': country.country.names['en'],
-                'countryCode': country.country.iso_code,
-                'region': city.subdivisions.most_specific.iso_code,
-                'regionName': city.subdivisions.most_specific.names['en'],
-                'city': city.city.names['en'],
-                'zip': city.postal.code if city.postal.code else "",
-                'lat': city.location.latitude if city.location.latitude else "",
-                'lon': city.location.longitude if city.location.longitude else "",
-                'timezone': city.location.time_zone if city.location.time_zone else "",    
+                "status": "success",
+                "ip": ip,
+                "country": (
+                    country.country.names["en"]
+                    if country.country.names.get("en")
+                    else ""
+                ),
+                "countryCode": (
+                    country.country.iso_code if country.country.iso_code else ""
+                ),
+                "region": (
+                    city.subdivisions.most_specific.iso_code
+                    if city.subdivisions.most_specific.iso_code
+                    else ""
+                ),
+                "regionName": (
+                    city.subdivisions.most_specific.names["en"]
+                    if city.subdivisions.most_specific.names.get("en")
+                    else ""
+                ),
+                "city": city.city.names["en"] if city.city.names.get("en") else "",
+                "zip": city.postal.code if city.postal.code else "",
+                "lat": city.location.latitude if city.location.latitude else "",
+                "lon": (city.location.longitude if city.location.longitude else ""),
+                "timezone": (
+                    city.location.time_zone if city.location.time_zone else ""
+                ),
             }
-        except:
-            return {
-                'status': 'not found',
-                'ip': ip,
-                'country': "",
-                'countryCode': "",
-                'region': "",
-                'regionName': "",
-                'city': "",
-                'zip': "",
-                'lat': "",
-                'lon': "",
-                'timezone': "",    
-            }
-            
+        except Exception as e:
+            return {"status": "error", "message": str(e)}
+
     def close(self):
         self.reader_city.close()
         self.reader_country.close()
-        
-# Path: app/main.py
